1039fadcb4f547571aa580f890e59d05f96bbddb
camierzwa73 <camierzwa73@gmail.com>
2017-08-28 15:51:14 -0400

    git remote add origin https://github.com/cmierzwa73/MierzwaRestaurant.git

---
+++ Restaurant/app/app.module.js
@@ -0,0 +1,7 @@
+﻿//uses an IIFE (immediately-invoked function expression) to immediately execute this code and also to encapsulate its scope
+(function () {
+    'use strict'; //is optional : triggers modern browsers to run the JavaScript in strict mode
+
+    angular.module('app', []); //registers the module with the Angular framework
+
+})();
---
+++ Restaurant/app/main.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app') //controller is registered with the app module
+        .controller('Main', main);
+
+    function main() {
+        var vm = this;
+        vm.food = 'pizza';
+    }
+
+})();
